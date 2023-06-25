bce1d6a3a8c9ca512a0275b0ce58a6d7dd4f5fb5
=Rafał Arnista <=r.arnista.445@studms.ug.edu.pl>
2023-06-25 20:30:13 +0200

    Adding validators for all models

--- Organizacja-budzetu-domowego-main/budzet_domowy/models.py
+++ Organizacja-budzetu-domowego-main/budzet_domowy/models.py
@@ -1,18 +1,26 @@
 from django.db import models
+from django.core import validators
+from django.core.validators import MinValueValidator
 
 class Person(models.Model):
-    imie = models.CharField(max_length=30)
-    nazwisko = models.CharField(max_length=30)
-    email = models.EmailField(max_length=250, blank=True)
+    imie = models.CharField(max_length=30, blank=False, validators=[
+        validators.MinLengthValidator(2, 'Imię powinno składać się przynajmniej z 2 znaków.'),validators.MaxLengthValidator(30, 'Imię może zawierać maksymalnie 30 znaków.')])
+    nazwisko = models.CharField(max_length=30, blank=False,validators=[
+        validators.MinLengthValidator(2, 'Nazwisko powinno składać się przynajmniej z 2 znaków.'),
+        validators.MaxLengthValidator(30, 'Nazwisko może zawierać maksymalnie 30 znaków.')
+    ])
+    email = models.EmailField(max_length=250, blank=True, validators=[
+        validators.EmailValidator('Podaj poprawny adres email.')
+    ])
 
     def __str__(self):
-        return f"{self.imie}"
+        return f"{self.imie} {self.nazwisko}"
 
     class Meta:
         verbose_name_plural = "Osoby"
 
 class lista_miesiecy(models.Model):
-    miesiac = models.CharField(max_length = 100, primary_key=True)
+    miesiac = models.CharField(max_length = 100, primary_key=True, blank=False)
 
     class Meta:
         verbose_name_plural = 'Miesiąc'
@@ -21,7 +29,7 @@ class lista_miesiecy(models.Model):
         return f"{self.miesiac}"
 
 class lista_lat(models.Model):
-    rok = models.CharField(max_length = 4, primary_key=True)
+    rok = models.CharField(max_length = 4, primary_key=True, blank=False)
 
     class Meta:
         verbose_name_plural = 'Rok'
@@ -30,7 +38,8 @@ class lista_lat(models.Model):
         return f"{self.rok}"
 
 class Kategoria(models.Model):
-    nazwa = models.CharField(max_length=50)
+    nazwa = models.CharField(max_length=50, blank=False, validators=[
+        validators.MinLengthValidator(2, 'Nazwa kategorii powinna składać się przynajmniej z 2 znaków.'),validators.MaxLengthValidator(30, 'Nazwa kategorii może zawierać maksymalnie 50 znaków.')])
     opis = models.TextField(blank=True)
     class Meta:
         verbose_name_plural = "Kategorie"
@@ -41,10 +50,11 @@ class Kategoria(models.Model):
 
 
 class Wydatki(models.Model):
-    kwota = models.FloatField()
-    kategoria = models.ForeignKey(Kategoria, on_delete=models.CASCADE, null=False)
-    miesiac = models.ForeignKey(lista_miesiecy, on_delete=models.CASCADE, null=False, default='')
-    rok = models.ForeignKey(lista_lat, on_delete=models.CASCADE, null=False, default='')
+    kwota = models.FloatField(blank=False, validators=[
+        MinValueValidator(0.0, 'Wartość musi być większa niż 0.')])
+    kategoria = models.ForeignKey(Kategoria, on_delete=models.CASCADE, blank=False, default='')
+    miesiac = models.ForeignKey(lista_miesiecy, on_delete=models.CASCADE, blank=False, default='')
+    rok = models.ForeignKey(lista_lat, on_delete=models.CASCADE, blank=False, default='')
     
     def __str__(self):
         return f"{self.kwota} PLN, {self.kategoria}, {self.miesiac}-{self.rok}"
@@ -54,10 +64,12 @@ class Wydatki(models.Model):
 
 
 class Przychody(models.Model):
-    nazwa_przychodu = models.CharField(max_length=100, null=False)
-    przychod = models.FloatField(null=False)
-    miesiac = models.ForeignKey(lista_miesiecy, on_delete=models.CASCADE, null=False, default='')
-    rok = models.ForeignKey(lista_lat, on_delete=models.CASCADE, null=False, default='')
+    nazwa_przychodu = models.CharField(max_length=50, blank=False, validators=[
+        validators.MinLengthValidator(2, 'Nazwa powinna składać się przynajmniej z 2 znaków.'),validators.MaxLengthValidator(50, 'Nazwa może zawierać maksymalnie 50 znaków.')])
+    przychod = models.FloatField(blank=False, validators=[
+        MinValueValidator(0.0, 'Wartość musi być większa niż 0.')])
+    miesiac = models.ForeignKey(lista_miesiecy, on_delete=models.CASCADE, blank=False, default='')
+    rok = models.ForeignKey(lista_lat, on_delete=models.CASCADE, blank=False, default='')
 
     class Meta:
         verbose_name_plural = "Przychody"
@@ -68,8 +80,10 @@ class Przychody(models.Model):
 
 
 class Cele(models.Model):
-    nazwa = models.CharField(max_length=200)
-    kwota = models.FloatField(null=False)
+    nazwa = models.CharField(max_length=50, blank=False, validators=[
+        validators.MinLengthValidator(2, 'Nazwa powinna składać się przynajmniej z 2 znaków.'),validators.MaxLengthValidator(50, 'Nazwa może zawierać maksymalnie 50 znaków.')])
+    kwota = models.FloatField(blank=False, validators=[
+        MinValueValidator(0.0, 'Wartość musi być większa niż 0.')])
     opis = models.TextField(blank=True)
 
     class Meta:
