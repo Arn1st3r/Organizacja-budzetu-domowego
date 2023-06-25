ef3df06e2f21f6f03a354275dfd685b4656d4150
=Rafał Arnista <=r.arnista.445@studms.ug.edu.pl>
2023-06-25 22:46:29 +0200

    new views for home page

--- Organizacja-budzetu-domowego-main/budzet_domowy/views.py
+++ Organizacja-budzetu-domowego-main/budzet_domowy/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .fields import WydatkiForm, PrzychodyForm
 from django.db.models import Sum
-from .models import Wydatki, Kategoria, lista_miesiecy, Przychody
+from .models import Wydatki, Kategoria, Przychody
 
 
 # Create your views here.
@@ -69,10 +69,54 @@ def wydatki_view(request):
         'kwota_per_month': kwota_per_month,
     }
     return render(request, 'wydatki.html', context)
+
 def statistics_view(request):
+    # Pobranie sumy przychodów dla każdego miesiąca
+    przychody_per_month = (
+        Przychody.objects
+        .values('miesiac')
+        .annotate(total_przychody=Sum('przychod'))
+    )
+
+    # Pobranie sumy wydatków dla każdego miesiąca
+    wydatki_per_month = (
+        Wydatki.objects
+        .values('miesiac')
+        .annotate(total_wydatki=Sum('kwota'))
+    )
+
+    # Obliczenie różnicy między przychodami a wydatkami dla każdego miesiąca
+    roznica_per_month = []
+    for przychod_entry in przychody_per_month:
+        miesiac = przychod_entry['miesiac']
+        przychod = przychod_entry['total_przychody'] or 0.0
+        wydatki_entry = next((entry for entry in wydatki_per_month if entry['miesiac'] == miesiac), {})
+        wydatki = wydatki_entry.get('total_wydatki', 0.0)
+        roznica = przychod - wydatki
+        roznica_per_month.append({'miesiac': miesiac, 'roznica': roznica})
+
+    # Reszta kodu
+    form = WydatkiForm(request.POST or None)
+    if form.is_valid():
+        form.save(commit=True)
+        form = WydatkiForm()
+
+    kwota_per_category = (
+        Wydatki.objects
+        .values('kategoria')
+        .annotate(total_kwota=Sum('kwota'))
+    )
+
+    kwota_per_category_with_names = []
+    for entry in kwota_per_category:
+        category_id = entry['kategoria']
+        category_name = Kategoria.objects.get(id=category_id).nazwa
+        entry['kategoria'] = category_name
+        kwota_per_category_with_names.append(entry)
+
     context = {
-        
+        'form': form,
+        'kwota_per_category': kwota_per_category_with_names,
+        'roznica_per_month': roznica_per_month,
     }
     return render(request, 'index.html', context)
-
-
